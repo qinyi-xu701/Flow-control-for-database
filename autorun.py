254bb5898c8d42ce57c91c8cd9cf40904c0dfcad
ludie <52432788+ludie@users.noreply.github.com>
2023-06-02 14:49:02 +0800

    add auto run all py file

---
+++ autorun.py
@@ -0,0 +1,17 @@
+import subprocess
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/Download_external_3files.py'], check=True)
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/concat.py'], check=True)
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/Upload_to_SQL.py'], check=True)
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/SGTransform.py'], check=True)
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/amend_data.py'], check=True)
+
+subprocess.run(['C:/Users/lulo/AppData/Local/Programs/Python/Python310/python.exe', 'c:/Users/lulo/GitHub/Flow-control-for-database/upload_PNFV_toSQL.py'], check=True)
+
+
+
+
