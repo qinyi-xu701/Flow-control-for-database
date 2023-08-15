a58e615bb4568085740b5bed9b43916a457ff607
dustinhsu1 <dustin.hsu1@hp.com>
2023-08-15 15:04:32 +0800

    some update

---
+++ autosendPNFV.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import timeit
+start = timeit.default_timer()
+import win32com.client
+from datetime import date
+from pathlib import Path
+import time
+
+
+# In[20]:
+
+
+home = Path.home()
+#讀取今天日期
+today = date.today().strftime("%Y%m%d")
+outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+# folder = Path(home, 'HP Inc', 'GPSTW SOP - 2021 日新')
+folder = Path(home, 'HP Inc','GPS TW Innovation - Documents','Users','GPS','Shortage management related (Ri Xin)')
+emailReceiverFolder = Path(home,'HP Inc', 'GPSTW SOP - 2021 日新', 'Project team')
+
+
+# In[21]:
+
+
+ccfile = Path(emailReceiverFolder, 'cc.txt')
+bccfile = Path(emailReceiverFolder, 'bcc.txt')
+
+
+# In[22]:
+
+
+with open(ccfile) as f:
+    cctext = f.read()
+
+with open(bccfile) as bf:
+    bcctext = bf.read()
+
+
+# In[23]:
+
+
+#打開新的email草稿
+mail = win32com.client.Dispatch("Outlook.Application").CreateItem(0)
+#讀取預設簽名檔
+mail.Display()
+signature = mail.Body
+
+
+# In[24]:
+
+
+#新增收件人以及信件主旨
+#mail.To = 'louis.lu2@hp.com'
+mail.To = ''
+mail.CC = cctext      
+mail.bcc = bcctext
+
+
+# In[25]:
+
+
+mail.Subject = 'PN FV description mapping table update_' + today
+
+#新增信件內容
+mail.HTMLBody = '<h3>This is HTML Body</h3>'
+mail.Body = "Hi receiver, \n\nPlease find latest PN FV description mapping table attached. Thanks." + signature
+#將剛才最後兩封信做為附件
+#mail.Attachments.Add(r'C:\Users\lulo\HP Inc\GPSTW SOP - 2021 日新\PN FV description mapping table_ALL.xlsx')
+print(Path(home, folder, 'PN FV description mapping table_ALL.xlsx'))
+
+mail.Attachments.Add(str(Path(folder, 'PN FV description mapping table_ALL.xlsx')))
+time.sleep(15)
+#寄信
+mail.Send()
+stop = timeit.default_timer()
+print('RunTime: ', stop - start, ' seconds') 
+
